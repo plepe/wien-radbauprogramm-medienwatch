8bd94504c896fdef8f122d38c7383e77a209b496
Stephan Bösch-Plepelits <skunk@xover.mud.at>
2023-04-10 13:10:27 +0200

    Convert to a specific class for MeinBezirk.at

--- import.js
+++ import.js
@@ -1,10 +1,12 @@
-const loadArticle = require('./src/loadArticle')
+const NewspaperMeinBezirk = require('./src/NewspaperMeinBezirk')
 const convert2Drupal = require('./src/convert2Drupal')
 const drupal = require('./src/drupal')
 
 const url = process.argv[process.argv.length - 1]
 
-loadArticle(url, (err, article) => {
+const newspaper = new NewspaperMeinBezirk()
+
+newspaper.loadArticle(url, (err, article) => {
   if (err) {
     console.error(err)
     process.exit(1)
