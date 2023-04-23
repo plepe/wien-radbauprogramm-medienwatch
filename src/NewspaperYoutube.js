caf04ce2aaf593286a79a0d6a2dc3bcddd70043a
Stephan Bösch-Plepelits <skunk@xover.mud.at>
2023-04-23 10:49:10 +0200

    Youtube: download description as well

--- src/NewspaperYoutube.js
+++ src/NewspaperYoutube.js
@@ -1,3 +1,4 @@
+const async = require('async')
 const child_process = require('child_process')
 const fs = require('fs')
 
@@ -11,20 +12,27 @@ module.exports = class NewspaperYoutube {
   }
 
   loadArticle (url, callback) {
-    child_process.execFile('youtube-dl', [url, '-o', 'video.mp4', '-S', 'res,ext:mp4:m4a', '--recode', 'mp4', '-f', 'bestvideo[height<=720]+bestaudio/best[height<=720]'], {
+    child_process.execFile('youtube-dl', [url, '-o', 'video.mp4', '--write-description', '-S', 'res,ext:mp4:m4a', '--recode', 'mp4', '-f', 'bestvideo[height<=720]+bestaudio/best[height<=720]'], {
       cwd: '/tmp'
     },
     (err) => {
       if (err) { return callback(err) }
 
-      fs.readFile('/tmp/video.mp4', (err, content) => {
-        if (err) { return callback(err) }
+      const entry = {}
+      async.parallel([
+        (done) => fs.readFile('/tmp/video.mp4', (err, content) => {
+          if (err) { return callback(err) }
 
-        const videos = [{ content, src: 'video.mp4' }]
-        const entry = { videos }
+          entry.videos = [{ content, src: 'video.mp4' }]
+          done()
+        }),
+        (done) => fs.readFile('/tmp/video.description', (err, content) => {
+          if (err) { return callback(err) }
 
-        callback(null, entry)
-      })
+          entry.content = content.toString()
+          done()
+        })
+      ], (err) => callback(err, entry))
     })
   }
 }
