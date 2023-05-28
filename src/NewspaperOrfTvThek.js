fdd506d423232e35e8ef45bf1cd254be4fbab3d7
Stephan Bösch-Plepelits <skunk@xover.mud.at>
2023-05-28 09:21:10 +0200

    NewspaperOrfTvThek: simplify code by using '--no-playlist'

--- src/NewspaperOrfTvThek.js
+++ src/NewspaperOrfTvThek.js
@@ -17,22 +17,7 @@ module.exports = class NewspaperOrfTvThek {
     const result = { url }
 
     async.waterfall([
-      (done) => childProcess.execFile('youtube-dl', ['--flat-playlist', '-J', url], {
-          cwd: config.tmpDir
-        },
-        (err, stdout) => {
-          if (err) { return done(err) }
-
-          const data = JSON.parse(stdout)
-
-          const entries = data.entries.filter(entry => entry.display_id === displayId)
-          const entry = entries[0]
-          result.content = entry.description
-          result.title = entry.title
-
-          done(null, entry)
-        }),
-      (entry, done) => childProcess.execFile('youtube-dl', [url, '-o', 'video.mp4', '--playlist-items', entry.playlist_index, '-S', 'res,ext:mp4:m4a', '--recode', 'mp4', '-f', 'bestvideo[height<=720]+bestaudio/best[height<=720]'], {
+      (done) => childProcess.execFile('youtube-dl', [url, '-o', 'video.mp4', '--no-playlist', '-S', 'res,ext:mp4:m4a', '--recode', 'mp4', '-f', 'bestvideo[height<=720]+bestaudio/best[height<=720]'], {
           cwd: config.tmpDir
         },
         (err) => done(err)),
