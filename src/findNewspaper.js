b7be5dbe3cc9ddcce1db3d6dde565c1661386609
Stephan Bösch-Plepelits <skunk@xover.mud.at>
2026-08-17 07:48:45 +0200

    Newspaper*.match(): may return numbers to indicate match priority

--- src/findNewspaper.js
+++ src/findNewspaper.js
@@ -1,7 +1,23 @@
 const allNewspapers = require('./allNewspapers')
 
 module.exports = function findNewspaper (url) {
-  const newspapers = allNewspapers.filter(n => n.match(url))
+  // match may return a likely-hood:
+  // true = yes, this is it
+  // 10 = almost perfect
+  // 1 = fall-back option
+  // 0/false = no
+  const newspapersMatch = allNewspapers.map(n => {
+    let m = n.match(url)
+    if (m === true) { m = 9999 }
+    if (m === false ) { m = 0 }
+    return [ m, n ]
+  })
+  const newspapers = newspapersMatch
+    .sort((a, b) => {
+      return b[0] - a[0]
+    })
+    .filter(v => v[0] > 0)
+    .map(v => v[1])
 
   return newspapers.length ? newspapers[0] : null
 }
