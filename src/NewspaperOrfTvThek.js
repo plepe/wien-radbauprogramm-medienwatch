72c43ccf72f64f68a203c82a8c3cf45be45c809e
Stephan Bösch-Plepelits <skunk@xover.mud.at>
2026-08-18 07:57:46 +0200

    NewspaperOrfTvThek: fixes

--- src/NewspaperOrfTvThek.js
+++ src/NewspaperOrfTvThek.js
@@ -5,19 +5,18 @@ const config = require('../config.json')
 
 module.exports = class NewspaperOrfTvThek {
   title () {
-    return 'W24'
+    return 'ORF'
   }
 
   match (url) {
-    return !!url.match(/^https:\/\/tvthek\.orf\.at\//)
+    return !!url.match(/^https:\/\/(on|tvthek)\.orf\.at\//)
   }
 
   loadArticle (url, node, callback) {
-    const displayId = url.match(/\/([0-9]+)$/)[1]
     const result = { url }
 
     async.waterfall([
-      (done) => childProcess.execFile('youtube-dl', [url, '-o', 'video.mp4', '--no-playlist', '-S', 'res,ext:mp4:m4a', '--recode', 'mp4', '-f', 'bestvideo[height<=720]+bestaudio/best[height<=720]'], {
+      (done) => childProcess.execFile('yt-dlp', [url, '-o', 'video.mp4', '--no-playlist', '-S', 'res,ext:mp4:m4a', '--recode', 'mp4', '-f', 'bestvideo[height<=720]+bestaudio/best[height<=720]'], {
           cwd: config.tmpDir
         },
         (err) => done(err)),
