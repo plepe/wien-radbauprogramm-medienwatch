aa47cb393bd2095e5f76886e50d945b9c75c91ee
Stephan Bösch-Plepelits <skunk@xover.mud.at>
2026-08-19 12:55:51 +0200

    NewspaperOrfTvThek: parse meta data from webpage

--- src/NewspaperOrfTvThek.js
+++ src/NewspaperOrfTvThek.js
@@ -1,21 +1,36 @@
 const fs = require('fs')
 const async = require('async')
 const childProcess = require('child_process')
+const sprintf = require('sprintf-js').sprintf
 const config = require('../config.json')
 const getTempDir = require('./getTempDir')
+const getSel = require('./getSel.js')
+const getSelAttr = getSel.attr
+const getSelText = getSel.text
 
 module.exports = class NewspaperOrfTvThek {
   title () {
     return 'ORF'
   }
 
-  match (url) {
+  matchUrlDocument ({url}) {
     return !!url.match(/^https:\/\/(on|tvthek)\.orf\.at\//)
   }
 
-  loadArticle (url, node, callback) {
+  loadArticleFromDocument ({url, document}, node, callback) {
     const [ fsPath, tmpDir ] = getTempDir()
-    const result = { url }
+    const result = {
+      url: getSelAttr(document, 'meta[property="og:url"]', 'content'),
+      title: getSelText(document, 'h1.title'),
+      medium: getSelText(document, 'p.profile'),
+    }
+
+    const m = getSelAttr(document, 'span.datetime-container time[datetime]', 'datetime').match(/^(\d{4})-(\d+)-(\d+)T(.*)$/)
+    result.date = sprintf('%04d-%02d-%02dT%s', m[1], m[2], m[3], m[4])
+
+    if (['Wien heute'].includes(result.medium)) {
+      result.type = 'editorial'
+    }
 
     async.waterfall([
       (done) => childProcess.execFile('yt-dlp', [url, '-o', 'video.mp4', '--no-playlist', '-S', 'res,ext:mp4:m4a', '--recode', 'mp4', '-f', 'bestvideo[height<=720]+bestaudio/best[height<=720]'], {
